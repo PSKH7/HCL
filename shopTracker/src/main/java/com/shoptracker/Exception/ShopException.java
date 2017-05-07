716ab98ceb873dadeaeaa51a642c06e9157ee8d4
PSKH7 <prasannakharche@gmail.com>
2017-05-07 13:33:23 +0530

    Update ShopException.java

--- shopTracker/src/main/java/com/shoptracker/Exception/ShopException.java
+++ shopTracker/src/main/java/com/shoptracker/Exception/ShopException.java
@@ -1,11 +1,10 @@
 package com.shoptracker.Exception;
 
 
-
+/* This class takes care of producing, when possible, a meaningful output in 
+ * case of error. It handles all the exceptions related to shop operations*/
 public class ShopException extends Exception {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 100L;
 
 	public ShopException(String message) {
